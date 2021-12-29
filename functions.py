7a293ef00297b8bfc5887827c71111a51bca53dd
MarryBye <58619102+MarryBye@users.noreply.github.com>
2021-12-29 11:10:00 +0200

    Add files via upload

--- functions.py
+++ functions.py
@@ -7,12 +7,16 @@ from datetime import datetime
 
 cmds = {}
 
-async def createCommand(cmd, desc, func, *args):
-    cmds[cmd] = [desc, func, *args]
+async def createCommand(cmd, desc, func, forAdm, *args):
+    cmds[cmd] = [desc, func, forAdm, *args]
     return
 
 async def addToLogFile(text, name):
-  with open('chat_logs_' + str(name) + '.txt', 'a+') as writer:
+  try:
+    os.mkdir('data/' + name)
+  except:
+    pass
+  with open('data/' + str(name) + '/chat_logs.txt', mode='a+', encoding='utf-8') as writer:
     textToLog = '[' + datetime.now().strftime("%d/%m/%Y - %H:%M:%S") + '] ' + text
     writer.seek(0)
     data = writer.read()
@@ -52,61 +56,64 @@ async def cmd_clear(m):
   await m.channel.send('**{0}** очистил **{1}** последних сообщений в канале!'.format(m.author.name, arg))
 
 async def cmd_setAdminRole(m):
-  
   guild = m.guild
   arg = await getNumbers(m.content)
   adminRole = guild.get_role(int(arg))
-  
   path = 'data/' + str(guild.id)
-  
   try:
     os.mkdir(path)
   except:
     pass
-  
   await save(path + '/settings_adminRole.txt', arg)
   await m.channel.send('Установил ' + adminRole.name + ' как **роль администратора**!')
   await m.delete()
   
 async def cmd_setNewsChannel(m):
-  
   guild = m.guild
   arg = await getNumbers(m.content)
   newsChannel = guild.get_channel(int(arg))
-  
   path = 'data/' + str(guild.id)
-  
   try:
     os.mkdir(path)
   except:
     pass
-  
   await save(path + '/settings_newsChannel.txt', arg)
   await m.channel.send('Установил ' + newsChannel.name + ' как **новостной канал**!')
   await m.delete()
   
 async def cmd_setWelcomeChannel(m):
-  
   guild = m.guild
   arg = await getNumbers(m.content)
   welcomeChannel = guild.get_channel(int(arg))
-  
   path = 'data/' + str(guild.id)
-  
   try:
     os.mkdir(path)
   except:
     pass
-  
   await save(path + '/settings_welcomeChannel.txt', arg)
   await m.channel.send('Установил ' + welcomeChannel.name + ' как **приветственный канал**!')
   await m.delete()
   
-async def getAdminRole():
-  pass
+async def getAdminRole(m):
+  guild = m.guild
+  path = 'data/' + str(guild.id)
+  fl = open(path + '/settings_adminRole.txt', 'r')
+  content = int(fl.read())
+  fl.close()
+  return content
 
-async def getNewsChannel():
-  pass
+async def getNewsChannel(m):
+  guild = m.guild
+  path = 'data/' + str(guild.id)
+  fl = open(path + '/settings_newsChannel.txt', 'r')
+  content = int(fl.read())
+  fl.close()
+  return content
 
-async def getWelcomeChannel():
-  pass
+async def getWelcomeChannel(m):
+  guild = m.guild
+  path = 'data/' + str(guild.id)
+  fl = open(path + '/settings_welcomeChannel.txt', 'r')
+  content = int(fl.read())
+  fl.close()
+  return content
--- main_bot.py
+++ main_bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import asyncio
+import os
 
 import functions as fnc
 
@@ -12,8 +13,22 @@ kostil = {}
 
 @client.event
 async def on_ready():
-    
-    print('Я гей')
+  try:
+    os.mkdir('data')
+  except:
+    pass
+  print('Я гей')
+  
+@client.event
+async def on_guild_join(guild):
+  path = 'data/' + str(guild.id)
+  try:
+    os.mkdir(path)
+    await fnc.save(path + '/settings_adminRole.txt', -1)
+    await fnc.save(path + '/settings_newsChannel.txt', -1)
+    await fnc.save(path + '/settings_welcomeChannel.txt', -1)
+  except:
+    pass
 
 @client.event
 async def on_message(msg):
@@ -22,12 +37,12 @@ async def on_message(msg):
   helpEmb = discord.Embed(title='Помощь (префикс - ">")', description='Ниже приведены все команды этого бота', color=0xFF5733)
     
   # Add commands here
-  await fnc.createCommand('help', 'Получить справку.', fnc.cmd_help, [msg, helpEmb])
-  await fnc.createCommand('ping', 'Проверить жив ли бот.', fnc.cmd_ping, [msg])
-  await fnc.createCommand('clear', 'Очистить последние сообщения в чате.', fnc.cmd_clear, [msg])
-  await fnc.createCommand('setAdminRole', 'Установить роль администратора.', fnc.cmd_setAdminRole, [msg])
-  await fnc.createCommand('setNewsChannel', 'Установить новостной канал.', fnc.cmd_setNewsChannel, [msg])
-  await fnc.createCommand('setWelcomeChannel', 'Установить канал входов.', fnc.cmd_setWelcomeChannel, [msg])
+  await fnc.createCommand('help', 'Получить справку.', fnc.cmd_help, False, [msg, helpEmb])
+  await fnc.createCommand('ping', 'Проверить жив ли бот.', fnc.cmd_ping, False, [msg])
+  await fnc.createCommand('clear', 'Очистить последние сообщения в чате.', fnc.cmd_clear, True, [msg])
+  await fnc.createCommand('setAdminRole', 'Установить роль администратора.', fnc.cmd_setAdminRole, True, [msg])
+  await fnc.createCommand('setNewsChannel', 'Установить новостной канал.', fnc.cmd_setNewsChannel, True, [msg])
+  await fnc.createCommand('setWelcomeChannel', 'Установить канал входов.', fnc.cmd_setWelcomeChannel, True, [msg])
     
     # Do not touch!!!
   for cmd in fnc.cmds:
@@ -35,31 +50,51 @@ async def on_message(msg):
   
   user = msg.author
   channel = msg.channel
-  messageIsPrivate = (str(channel.type) == 'private')
+  messageIsPrivate = str(channel.type) == 'private'
   guild = msg.guild
   content = msg.content
   
   # Not commands
   
+  if messageIsPrivate:
+    await fnc.addToLogFile('[PRIVATE] {0}: {1}'.format(user.name, content), 'PRIVATE_LOGS')
+    return
+  
+  adminRoleID = await fnc.getAdminRole(msg)
+  adminRole = guild.get_role(adminRoleID)
+  
+  newsChannelID = await fnc.getNewsChannel(msg)
+  newsChannel = guild.get_role(newsChannelID)
+  
+  welcomeChannelID = await fnc.getWelcomeChannel(msg)
+  welcomeChannel = guild.get_role(welcomeChannelID)
+  
+  await fnc.addToLogFile('[{0}] {1}: {2}'.format(channel.name, user.name, content), guild.id)
+  
   if msg.author.bot:
     return
-
-  if messageIsPrivate and content != '':
-    await fnc.addToLogFile('[PRIVATE] {0}: {1}'.format(user.name, content), 'private')
-    return
-
+  
   if content[0] != '>':
-    await fnc.addToLogFile('[{0}] {1}: {2}'.format(channel.name, user.name, content), guild.id)
+    if channel.id == newsChannelID:
+      await msg.reply('News')
+      return
     
   # Commands
   
   for cmd in fnc.cmds:
     command = prefix + cmd
     if command == content[0:len(command)]:
-      await fnc.cmds[cmd][1](*fnc.cmds[cmd][2])
-      return
-  
-  # Error no command
+      if fnc.cmds[cmd][2]:
+        if adminRole in user.roles or user.id == guild.owner.id:
+          await fnc.cmds[cmd][1](*fnc.cmds[cmd][3])
+          return
+        else:
+          await msg.reply('У вас нет доступа к этой команде!')
+          return
+      else:
+        await fnc.cmds[cmd][1](*fnc.cmds[cmd][3])
+        return
+    
   await msg.reply('Такой команды не существует!')
   return
     
@@ -71,4 +106,4 @@ async def on_member_join(member):
 async def on_member_remove(member):
     pass
 
-client.run('')
+client.run('OTI1MzMxNDg3MDUyNjg5NDM5.YcrkGg.5CUAzUv3ERJCJTwRdDIWyk_pBXk')
